608ad41a5167b5c1d2d37dc8a1a0b2cf2adc1773
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-27 07:49:19 +0530

    Added: FirstName Valid Expression

--- src/com/program/RegexProgram.java
+++ src/com/program/RegexProgram.java
@@ -1,7 +1,18 @@
 package com.program;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexProgram {
+	
+	public void regexRunner(String regexExpression,String userIP) {
+		boolean valid=Pattern.compile(regexExpression).matcher(userIP).matches();
+		System.out.println(valid);
+		}
+	
 public static void main(String[] args) {
 	System.out.println("Welcome To Regex Program");
+	RegexProgram obj=new RegexProgram();
+	obj.regexRunner("^[A-Z]+[a-z]{2,}", "Ashu");
 }
 }
