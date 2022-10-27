895b7dc0d3c82e5a0b083d2ebb885baaffc9d968
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-27 10:55:54 +0530

    Refactor : User Email Id Valid and Invalid Expression

--- src/com/program/RegexProgram.java
+++ src/com/program/RegexProgram.java
@@ -7,12 +7,35 @@ public class RegexProgram {
 	
 	public void regexRunner(String regexExpression,String userIP) {
 		boolean valid=Pattern.compile(regexExpression).matcher(userIP).matches();
-		System.out.println(valid);
+		System.out.println(userIP+" is "+valid);
 		}
 	
 public static void main(String[] args) {
 	System.out.println("Welcome To Regex Program");
 	RegexProgram obj=new RegexProgram();
-	obj.regexRunner("^[A-Z]+[A-Za-z]{7,}[@#$&][1][0-9]+$", "Password@12345");
+	System.out.println("--------------------True Cases------------------------");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc@yahoo.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc.100@yahoo.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$","abc111@abc.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc-100@abc.net");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc.100@abc.com.au");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc@1.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc@gmail.com.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc+100@gmail.com");
+	System.out.println("----------------------False Cases---------------------");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc@.com.my");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc123@gmail.a");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "Password@12345");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc123@.com.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", ".abc@abc.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc()*@gmail.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc@%*.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc..2002@gmail.com");
+	obj.regexRunner("^[a-zA-Z]+([.+-]?[A-Za-z0-9])*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc.@gmail.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc@abc@gmail.com");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc@gmail.com.1a");
+	obj.regexRunner("^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$", "abc@gmail.com.aa.au");
+
 }
 }
